0ed74221be6f92e60cee3f0337857e389ac802dd
simma <simranjitsingh@simranjits-MacBook-Air.local>
2026-02-23 22:39:01 +0530

    Fixed

--- sms_panel/sms_panel/settings.py
+++ sms_panel/sms_panel/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 # Applications
 INSTALLED_APPS = [
