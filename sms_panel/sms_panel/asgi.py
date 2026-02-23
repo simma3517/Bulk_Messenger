2308792371454932067fd82d97a5ddda1645ae64
simma <simranjitsingh@simranjits-MacBook-Air.local>
2026-02-23 19:45:03 +0530

    Fixed DJANGO_SETTINGS_MODULE path

--- sms_panel/sms_panel/asgi.py
+++ sms_panel/sms_panel/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_panel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_panel.sms_panel.settings')
 
 application = get_asgi_application()
