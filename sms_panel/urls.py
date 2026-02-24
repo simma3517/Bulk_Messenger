4935e89788171b9b9960ae41228a327166e10fd6
simma <simranjitsingh@simranjits-MacBook-Air.local>
2026-02-24 16:34:59 +0530

    Fixed structure for Render

--- sms_panel/urls.py
+++ sms_panel/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('login')),  # 👈 ADD THIS
-    path('', include('accounts.urls')),
-    path('campaigns/', include('campaigns.urls')),
 ]
