4d9214769be637f5c4bdb9d9c68c1d5ae820d6b2
simma <simranjitsingh@simranjits-MacBook-Air.local>
2026-02-24 18:51:26 +0530

    Django project upload

--- sms_panel/Bulk_Messenger/sms_panel/manage.py
+++ sms_panel/Bulk_Messenger/sms_panel/manage.py
@@ -6,6 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_panel.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,6 +16,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
--- sms_panel/settings.py
+++ sms_panel/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+'campaigns',
+'core',
+'reports',
+'wallet',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'sms_panel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.User'
